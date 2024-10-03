ea290c24cb8141c3644aa60ed137b5b456d6cd6e
bastien.rossiaud <bastien.rossiaud@gmail.com>
2024-10-03 11:21:28 +0200

    versions of the pendu that works (upgrades to do)

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@
 bastien.rossiaud@gmail.com
 03/10/2024
 objective : program that contains functions to realise a "pendu"
-to do : main program
+to do : bonnes pratiques
 """
 
 import random as r
@@ -17,32 +17,41 @@ def choice():
     file = open("mots.txt",'r')
     contenu = file.read()
     mots = contenu.split()
-    alea = r.random(len(mots))
+    alea = r.randint(1, len(mots))
     return mots[alea]    
 
 def affichage_mot(mot, affiche, lettre): 
     """fonction qui effectue l'affichage du mot
     in : str (un mot); lettre un str
     out : str (le mot en train d'etre cherché)"""
-    for i, el in enumerate(mot) : 
+    nouv_mot = ""
+    for i, el in enumerate(mot) :  
          if el == lettre :
               affiche[i] = lettre
     for el in affiche : 
          nouv_mot += el
-    return nouv_mot
+    return nouv_mot,affiche
 
 def first_print(mot):
-        affiche = [mot[0],"___" * len(mot) - 1]
-        return affiche
+     nouv_mot = ""
+     affiche = [mot[0]]
+     for i in range(len(mot)-1):
+          affiche.append(" ___ ")
+     for el in affiche : 
+          nouv_mot += el
+     return nouv_mot,affiche
+
 
 def verif_lettre(mot, lettre):
      return lettre in mot
 
 def verif_mot_complet(affiche) : 
-     return "___" in affiche
+     return " ___ " in affiche
 
 def lettre_proposee(lettre):
      string = str(input("Lettre deja proposée choississez en une autre : "))
      if string != lettre : 
           return string
 
+def verif_nbre_vies(lives):
+     return lives == 0
--- main.py
+++ main.py
@@ -7,20 +7,35 @@ objective : main program for the pendu
 to do : main program
 """
 
-import functions as f
+import functions as fct
 
 #déclaration des variables
+def jouer():
+    lives = 8
+    mot = fct.choice()
+    nouv_mot, affiche = fct.first_print(mot)
+    memoire_lettres = []
 
-lives = 8
-mot = f.choice()
-affiche = f.first_print(mot)
-memoire_lettres = []
+    print(nouv_mot)
 
-print(affiche)
+    while fct.verif_mot_complet(affiche) : 
+        lettre = str(input("Proposez une lettre : "))
+    # if lettre in memoire_lettres : 
+        #    memoire_lettres.append(f.lettre_proposee(lettre))
+        if fct.verif_lettre(mot, lettre):
+            nouv_mot, affiche = fct.affichage_mot(mot,affiche,lettre)
+            print (nouv_mot)
+        else : 
+            print(nouv_mot)
+            if fct.verif_nbre_vies(lives) == True :
+                print(f"vous avez perdu, le mot était {mot}")
+                raise IndexError
+            else :
+                lives -= 1
 
-while f.verif_mot_complet == True : 
-    lettre = str(input("Proposez une lettre : "))
-    if lettre in memoire_lettres : 
-        string = f.lettre_proposee(lettre)
-    if f.verif_lettre(mot, lettre):
-            pass
+    print("vous avez gagné !")
+
+
+jouer()
+if str(input("rejouer ? ")) == "oui" :
+    jouer()
